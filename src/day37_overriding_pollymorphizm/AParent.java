86bb5540528e90bf76716dcd52cc5ec98440ea62
suhabolgi <119219230+suhabolgi@users.noreply.github.com>
2023-08-15 23:28:20 +0300

    37. dersin sonu

---
+++ src/day37_overriding_pollymorphizm/AParent.java
@@ -0,0 +1,17 @@
+package day37_overriding_pollymorphizm;
+
+public class AParent {
+
+    public final void finalMethod(){
+        System.out.println("Bu bir final method");
+    }
+
+    public static void staticMethod(){
+        System.out.println("Bu bir static method");
+    }
+
+    private void privateMethod(){
+        System.out.println("Bu bir private method");
+    }
+
+}
